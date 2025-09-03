3245e94210c832a064b5a075224d47458b976116
Masaldoter <aldoyagu@gmail.com>
2025-09-02 18:29:54 -0600

    Creación de Proyecto e Importación de Librerias
    Aquí, creamos el proyecto, agregamos las librerías únicamente necesarias.
    También agregamos la configuración a nuestra base de datos de postgresql en nuestro application.properties

---
+++ Backend/IntegraOne/src/main/java/IntegraOne/IntegraOneApplication.java
@@ -0,0 +1,13 @@
+package IntegraOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegraOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegraOneApplication.class, args);
+	}
+
+}
---
+++ Backend/IntegraOne/src/test/java/IntegraOne/IntegraOneApplicationTests.java
@@ -0,0 +1,13 @@
+package IntegraOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegraOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
